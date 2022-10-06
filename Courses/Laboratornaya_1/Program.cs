0fdbfffb225462f77cf3014c54c7c80e0856a7db
Sapelnikova Elena <hmamaeva@gmail.com>
2022-10-06 21:41:48 +0700

    Лабораторная 1

---
+++ Courses/Laboratornaya_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Laboratornaya_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствуйте, введите температуру в градусах Цельсия");
+            double celsTemp = Convert.ToDouble (Console.ReadLine());
+            double farTemp = celsTemp * 9 / 5 + 32;
+            Console.WriteLine("Температура в Фаренгейтах : {0:F6}", farTemp);
+            Console.ReadLine();
+        }
+    }
+}
